26a55af57bfdde85b4f27edd2b9fe7011c08184e
Ori Fine <ori.fine@post.runi.ac.il>
2025-10-31 00:03:24 +0200

    improved text 1.3

--- Bill3.java
+++ Bill3.java
@@ -8,7 +8,7 @@ public class Bill3 {
         double total = Double.parseDouble(args[3]); // fourth argument, converted to double
 	    double share = total / 3; // how much everyone owes
 	    double RoundedShare = Math.ceil(share); // rounded up to the next dollar
-System.out.println("Dear " + name3 + ", "  + name2 + ", and " + name1 + ": pay " + RoundedShare + " Sheckels each.");
+System.out.println("Dear " + name3 + ", "  + name2 + ", and " + name1 + ": pay " + RoundedShare + " Shekels each.");
 
 
 	}
