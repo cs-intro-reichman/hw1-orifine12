10602e6c3b209c4e6ce47146679a026682fd8d52
Ori Fine <ori.fine@post.runi.ac.il>
2025-10-30 23:45:06 +0200

    made changes to name order

--- Bill3.java
+++ Bill3.java
@@ -1,14 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+		
 		String name1 = args[0];   // take the first argument
         String name2 = args[1];   // second argument
         String name3 = args[2];   // third argument
         double total = Double.parseDouble(args[3]); // fourth argument, converted to double
 	    double share = total / 3; // how much everyone owes
 	    double RoundedShare = Math.ceil(share); // rounded up to the next dollar
-System.out.println("Dear" + " " + name1 + " "  + name1 + " " + name1 + " " + "please pay each:" + RoundedShare);
+System.out.println("Dear" + " " + name3 + " "  + name2 + " " + name1 + " " + "pay " + RoundedShare + "sheckels each.");
 
 
 	}
