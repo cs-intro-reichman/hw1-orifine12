d1dca3527ae4cefcb741d7324b46afd37d783d56
Ori Fine <ori.fine@post.runi.ac.il>
2025-10-31 12:57:58 +0200

    minor changes with double val

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,9 @@ public class FVCalc {
 double currentValue = Double.parseDouble(args [0]); // initial amount	
 double rate = Double.parseDouble(args [1]); // annual interest rate
 int n = Integer.parseInt(args [2]); // number of years)
-double futureValue = currentValue * Math.pow (1 + rate, n); 
-double DevidedFutureValue = futureValue/100;
-System.out.println("After " + n + " years, $" + (int) currentValue + " saved at " + rate + "% will yield $" + (int) DevidedFutureValue); //printing the future value as an Int 
+double futureValueInput = currentValue * Math.pow (1 + rate, n); 
+double Devide = 100.0;
+double CorrectFutureValue = futureValueInput/Devide; //dividing by 100 to get the correct future value
+System.out.println("After " + n + " years, $" + (int) currentValue + " saved at " + rate + "% will yield $" + (int) CorrectFutureValue); //printing the future value as an Int 
 	}
 }
