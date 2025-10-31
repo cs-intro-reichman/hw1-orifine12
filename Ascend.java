027945698ed93f15f66323c2e01909325d2b287f
Ori Fine <ori.fine@post.runi.ac.il>
2025-10-31 20:51:00 +0200

    asc V1

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,17 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int mainNumber = Integer.parseInt (args[0]);
+		int a = (int) (Math.random() * mainNumber);
+		int b = (int) (Math.random() * mainNumber);
+		int c = (int) (Math.random() * mainNumber);	
+int max1 = Math.max (a, b);
+int sum = a + b + c;
+int maxWinner = Math.max (max1, c);
+int min1 = Math.min (a, b);
+int minWinner = Math.min (min1, c);
+int midWinner = sum - maxWinner - minWinner;
+System.out.println (minWinner + " " + midWinner + " " + maxWinner);
+		
 	}
 }
