c04b1f28c04eef3447f4f5e024341db9526f541a
Ori Fine <ori.fine@post.runi.ac.il>
2025-10-31 13:04:23 +0200

    fvc 1.2

--- FVCalc.java
+++ FVCalc.java
@@ -2,11 +2,10 @@
 public class FVCalc {
 	public static void main(String[] args){
 double currentValue = Double.parseDouble(args [0]); // initial amount	
-double rate = Double.parseDouble(args [1]); // annual interest rate
+double rate = Double.parseDouble(args [1]);
+double correctRate = rate/100.0; // annual interest rate
 int n = Integer.parseInt(args [2]); // number of years)
-double futureValueInput = currentValue * Math.pow (1 + rate, n); 
-double Devide = 100.0;
-double CorrectFutureValue = futureValueInput/Devide; //dividing by 100 to get the correct future value
-System.out.println("After " + n + " years, $" + (int) currentValue + " saved at " + rate + "% will yield $" + (int) CorrectFutureValue); //printing the future value as an Int 
+double futureValue = currentValue * Math.pow (1 + correctRate, n); 
+System.out.println("After " + n + " years, $" + (int) currentValue + " saved at " + rate + "% will yield $" + (int) futureValue); //printing the future value as an Int 
 	}
 }
